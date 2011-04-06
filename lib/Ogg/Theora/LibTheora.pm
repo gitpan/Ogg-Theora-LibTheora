f3cb4193ea64bbac50fee7abc136d895eba1c1e4
Vigith Maurice <vigith@cpan.org>
2011-04-06 15:12:23 +0000

    Import of VIGITH/Ogg-Theora-LibTheora-0.02 from CPAN.
    gitpan-cpan-distribution: Ogg-Theora-LibTheora
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         VIGITH/Ogg-Theora-LibTheora-0.02.tar.gz
    gitpan-cpan-author:       VIGITH
    gitpan-cpan-maturity:     released

--- lib/Ogg/Theora/LibTheora.pm
+++ lib/Ogg/Theora/LibTheora.pm
@@ -185,7 +185,7 @@ our @EXPORT = qw(
     TH_RATECTL_DROP_FRAMES
 );
 
-our $VERSION = '0.01.2';
+our $VERSION = '0.02';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
